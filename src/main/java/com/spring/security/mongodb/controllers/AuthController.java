1f4ca8bb2a96e141df0065888566293c0a856a8c
hyder <hyderabbas89850@gmail.com>
2023-06-25 01:52:26 +0530

    testing commit

--- src/main/java/com/spring/security/mongodb/controllers/AuthController.java
+++ src/main/java/com/spring/security/mongodb/controllers/AuthController.java
@@ -38,6 +38,7 @@ import com.spring.security.mongodb.security.services.UserDetailsImpl;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+	
   @Autowired
   AuthenticationManager authenticationManager;
 
