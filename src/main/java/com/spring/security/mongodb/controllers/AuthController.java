46369e31d2c7c60e8fe34c19deb700e32722bfc6
hyder <hyderabbas89850@gmail.com>
2023-06-25 01:47:33 +0530

    removed unused imports

--- src/main/java/com/spring/security/mongodb/controllers/AuthController.java
+++ src/main/java/com/spring/security/mongodb/controllers/AuthController.java
@@ -9,7 +9,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
